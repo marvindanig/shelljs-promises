b7eb07abc4ec31ed43b8808e0a04ec29acda81d2
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-07-04 13:00:51 -0600

    Play nice with standard node

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 // @flow
 /* eslint-disable consistent-return, max-len, spaced-comment */
 
-import sh from "shelljs";
+const sh = require("shelljs");
 /*::
 import type { // eslint-disable-line no-duplicate-imports
   OptExec,
 } from "shelljs";
 */
 
-export function ShellString(stdout/*: string | string[]*/)/*: Promise<ShellJS$String>*/ {
+function ShellString(stdout/*: string | string[]*/)/*: Promise<ShellJS$String>*/ {
   return Promise.resolve(new sh.ShellString(stdout));
 }
 
-export function execp(cmd/*: string*/, options/*: OptExec*/ = {})/*: Promise<ShellJS$String>*/ {
+function execp(cmd/*: string*/, options/*: OptExec*/ = {})/*: Promise<ShellJS$String>*/ {
   return new Promise((resolve) => {
     sh.exec(cmd, options, (code/*: number*/, stdout/*: string*/, stderr/*: string*/) => {
       resolve(new sh.ShellString(stdout, stderr, code));
     });
   });
 }
+
+module.exports = {
+  ShellString,
+  execp,
+};
