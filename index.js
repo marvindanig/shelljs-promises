06a17c508ab35bdb38fd9f84d4440bf718ab3e0d
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-07-05 14:46:01 -0600

    Use local type

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const sh = require("shelljs");
 /*::
 import type {
   OptExec,
+  ShellString,
 } from "shelljs";
 */
 
-function shellString(stdout/*: string | string[]*/ = "")/*: Promise<ShellJS$String>*/ {
+function shellString(stdout/*: string | string[]*/ = "")/*: Promise<ShellString>*/ {
   return Promise.resolve(new sh.ShellString(stdout));
 }
 
-function exec(cmd/*: string*/, options/*: OptExec*/ = {})/*: Promise<ShellJS$String>*/ {
+function exec(cmd/*: string*/, options/*: OptExec*/ = {})/*: Promise<ShellString>*/ {
   return new Promise((resolve) => {
     sh.exec(cmd, options, (code/*: number*/, stdout/*: string*/, stderr/*: string*/) => {
       resolve(new sh.ShellString(stdout, stderr, code));
